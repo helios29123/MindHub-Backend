3e9fcc5ce0ee535dae25b34f76755f6781ca1954
MinhDangWBE <dominhdang3010@gmail.com>
2026-06-07 20:37:12 +0700

    feat(user): implement USER-02 cap nhat ho so ca nhan

--- BE/app/Repositories/UserRepository.php
+++ BE/app/Repositories/UserRepository.php
@@ -41,4 +41,31 @@ class UserRepository
 
         return $user->refresh();
     }
+    public function findPublicProfileById(int $id): User
+    {
+        return User::query()
+            ->select([
+                'id',
+                'full_name',
+                'email',
+                'phone',
+                'role',
+                'status',
+                'email_verified_at',
+            ])
+            ->whereKey($id)
+            ->firstOrFail();
+    }
+
+    public function updateProfileById(int $id, array $data): bool
+    {
+        $allowedData = array_intersect_key($data, array_flip([
+            'full_name',
+            'phone',
+        ]));
+
+        return User::query()
+            ->whereKey($id)
+            ->update($allowedData);
+    }
 }
