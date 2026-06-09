142cee4a4ac5411f1b43c49447c300f7c1404182
Noahntp <dauxanh1234x@gmail.com>
2026-06-09 19:19:50 +0700

    feat(catalog): implement CAT-03 tim kiem khoa hoc (#19)

--- BE/app/Http/Controllers/CatalogController.php
+++ BE/app/Http/Controllers/CatalogController.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Http\Controllers;
 use App\Http\Requests\Catalog\CatalogListRequest;
-// use App\Http\Requests\Catalog\CourseSearchRequest;
 // use App\Http\Requests\Catalog\CourseSortRequest;
+use App\Http\Requests\Catalog\CourseSearchRequest;
 // use App\Http\Requests\Catalog\SearchSuggestionRequest;
-// use App\Http\Resources\Catalog\CatalogCourseResource;
+use App\Http\Resources\Catalog\CatalogCourseResource;
 use App\Http\Resources\Catalog\CategoryResource;
 // use App\Http\Resources\Catalog\FeaturedInstructorResource;
 use App\Http\Resources\Catalog\HomeResource;
@@ -17,7 +17,6 @@ class CatalogController extends Controller
     public function __construct(private readonly CatalogService $catalogService)
     {
     }
-
     public function home(CatalogListRequest $request): JsonResponse
     {
         $homeData = $this->catalogService->home($request->validated(), $request->user());
@@ -29,5 +28,11 @@ class CatalogController extends Controller
         $categories = $this->catalogService->categories($request->validated());
 
         return ApiResponse::paginated(CategoryResource::collection($categories), $categories);
+    }
+    public function searchCourses(CourseSearchRequest $request): JsonResponse
+    {
+        $courses = $this->catalogService->searchCourses($request->validated(), $request->user());
+
+        return ApiResponse::paginated(CatalogCourseResource::collection($courses), $courses);
     }
 }
