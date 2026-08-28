3aef1faad58ac2f08506cc16768105627bc87d8a
helios29123 <203520403+helios29123@users.noreply.github.com>
2026-08-28 15:58:28 +0700

    feat: implement CourseVideoSeeder to import and seed course video lessons from JSON data

--- BE/database/seeders/DatabaseSeeder.php
+++ BE/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             CommissionRuleSeeder::class,
             CatalogCatDatabaseSeeder::class,
+            CourseVideoSeeder::class,
         ]);
     }
 }
